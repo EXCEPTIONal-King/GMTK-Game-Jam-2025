089bc05d79d89306e0b6efa42e4e3965cf94a3d6
Ben Palmer <bencolep@gmail.com>
2025-07-31 06:48:03 -0400

    defining a few classes (grid system stuff)

---
+++ Assets/Scripts/ConveyorUnit.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ConveyorUnit : MonoBehaviour
+{
+
+    int[] pos;
+    int[] target_pos; //position going to
+    int[] prior_pos; //position feeding into it, used for easy turn around, maybe should do it differently
+    //need someway to turn corner different than straight-away
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,47 @@
+using System.Net.Mail;
+using UnityEngine;
+
+//Could be made obsolete if conveyors and conveyor units handle the objects on them. I'll assume we use this for now though
+public class GridSystem : MonoBehaviour
+{
+
+    [SerializeField] int horizontal_size;
+    [SerializeField] int vertical_size;
+
+    //contain all data for a grid position
+    class Location
+    {
+        Box box;
+        ConveyorUnit conveyor;
+        //Recipient
+        //IObstacle or obj
+        //Wifi beam ...
+    }
+
+    Location[][] grid;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //generate grid with a Location object in each location
+        grid = new Location[vertical_size][];
+
+        for (int i = 0; i < vertical_size; i++)
+        {
+            for (int j = 0; j < horizontal_size; j++)
+            {
+                grid[i][j] = new Location();
+            }
+        }
+
+        //populate other objects into their locations using their initial pos values in a scene
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
