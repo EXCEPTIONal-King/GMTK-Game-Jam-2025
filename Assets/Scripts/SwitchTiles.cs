ad98f93453ad4e7a89537d08ac244e3f156aea33
Ben Palmer <bencolep@gmail.com>
2025-08-01 23:52:51 -0400

    switch file created

---
+++ Assets/Scripts/SwitchTiles.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SwitchTiles : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    GridSystem.Location[] targets_locations = new GridSystem.Location[2];
+    [SerializeField] Vector2Int[] targets = new Vector2Int[2];
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SwitchBoxes()
+    {
+        
+    }
+}
