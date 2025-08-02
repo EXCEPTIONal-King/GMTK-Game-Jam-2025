10d521be22cd4a5fe5f63a69dd48179f2f69737a
Ben Palmer <bencolep@gmail.com>
2025-08-01 23:05:08 -0400

    moved grid init to Awake to pre-empt adding things to grid. Conveyor now handles adding its segments to the grid, reverse all conveyors avoids null reference for box. All reverse and Selected reverse now implemented. Scene now has duplicate track to test this

---
+++ Assets/Scripts/ConveyorSelector.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ConveyorSelector : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    int index = 0;
+    Conveyor selected;
+    Conveyor[] all_conveyors;
+    [SerializeField] int all_conveyor_reverses;
+
+    void Start()
+    {
+        all_conveyors = FindObjectsByType<Conveyor>(FindObjectsSortMode.None);
+        selected = all_conveyors[index];
+
+        Controls con = new Controls();
+        con.GamePlay.Enable();
+        con.GamePlay.ShiftConveyorSelection.performed += ShiftConveyor;
+        con.GamePlay.ReverseSelected.performed += ReverseSelected;
+        con.GamePlay.Reverse.performed += ReverseAll;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ShiftConveyor(InputAction.CallbackContext context)
+    {
+        index++;
+        if (index >= all_conveyors.Length) index = 0;
+        selected = all_conveyors[index];
+
+
+    }
+
+    void ReverseSelected(InputAction.CallbackContext context)
+    {
+        selected.ReverseRotation(false);
+    }
+    
+    public void ReverseAll(InputAction.CallbackContext context)
+    {
+        if (all_conveyor_reverses <= 0) return;
+
+        all_conveyor_reverses--;
+        foreach (Conveyor conveyor in all_conveyors)
+        {
+            conveyor.ReverseRotation(true);
+        }
+    }
+
+}
