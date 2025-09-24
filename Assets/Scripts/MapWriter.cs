786d765bb016515bb61ae44538b703fe34a85a68
Ben Palmer <bencolep@gmail.com>
2025-09-24 16:32:49 -0400

    start prototype of map builder - should move to other branch

---
+++ Assets/Scripts/MapWriter.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class MapWriter : MonoBehaviour
+{
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // (location and object format)
+    // each object has prefab listed (data structure?)
+    // account for multiple things on tile (receiver + base tile or Conveyro + box)
+    // 
+    void ReadLayout()
+    {
+
+    }
+
+    // multiply location by scale of tile
+    void BuildMap()
+    {
+        
+
+
+    }
+
+}
